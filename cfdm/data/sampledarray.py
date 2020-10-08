e0d31e6bfbd187226ed3a9ee6a202f3f925d4020
David Hassell <david.hassell@ncas.ac.uk>
2020-10-08 14:12:03 +0100

    array

--- cfdm/data/sampledarray.py
+++ cfdm/data/sampledarray.py
@@ -12,9 +12,15 @@ class SampledArray(abstract.Array):
     .. versionadded:: (cfdm) TODO
 
     '''
+    _interpolated_datatype = {
+        'linear': numpy.dtype(float),
+        'bilinear': numpy.dtype(float),
+    }
+    
     def __init__(self, shape=None, size=None, ndim=None,
-                 interpolation=None, tie_points=None,
-                 tie_point_indices=None, tie_point_offsets=None):
+                 sampled_dimensions=None, interpolation=None,
+                 tie_points=None, tie_point_indices=None,
+                 tie_point_offsets=None, coefficients=None):
         '''**Initialization**
 
     :Parameters:
@@ -28,29 +34,31 @@ class SampledArray(abstract.Array):
         ndim: `int`
             The number of uncompressed array dimensions
 
+        sampled_dimensions: sequence of `int`
+            TODO
+
         interpolation: `str`
+            TODO
 
         tie_points: sequence of `TiePoint`
-            TODO The count variable required to uncompress the data,
-            corresponding to a CF-netCDF count variable.
+            TODO
 
         tie_point_indices: sequence of `TiePointIndex` or `None`
-            TODO The count variable required to uncompress the data,
-            corresponding to a CF-netCDF count variable.
+            TODO
 
         tie_point_offsets: sequence of `TiePointOffset` or `None`, optional
-            TODO The count variable required to uncompress the data,
-            corresponding to a CF-netCDF count variable.
-
+            TODO
         coefficients: sequence of `InterpolationCoefficient`, optional
+            TODO
 
         '''
-        super().__init__(compressed_array=compressed_array,
-                         shape=shape, size=size, ndim=ndim,
-                         count_variable=count_variable,
-                         index_variable=index_variable,
-                         compression_type='ragged indexed contiguous',
-                         compressed_dimension=0)
+        super().__init__(shape=shape, size=size, ndim=ndim,
+                         sampled_dimensions=sampled_dimensions,
+                         interpolation=interpolation,
+                         tie_points=tie_points,
+                         tie_points_indices=tie_points_indices,
+                         tie_points_offsets=tie_points_offsets,
+                         compression_type='sampled')
 
     def __getitem__(self, indices):
         '''x.__getitem__(indices) <==> x[indices]
@@ -73,8 +81,12 @@ class SampledArray(abstract.Array):
     .. versionadded:: (cfdm) TODO
 
         '''
-        return self.get_subspace(self._get_component('array'), indices,
-                                 copy=True)
+        # ------------------------------------------------------------
+        # Method: Uncompress the entire array and then subspace it
+        # ------------------------------------------------------------
+
+        # Initialise the un-sliced uncompressed array
+        uarray = numpy.ma.masked_all(self.shape, dtype=self.dtype)
 
     # ----------------------------------------------------------------
     # Attributes
@@ -93,14 +105,181 @@ class SampledArray(abstract.Array):
     <type 'numpy.dtype'>
 
         '''
+        datatype = self._interpolated_datatype.get(self.get_interpolation())
+        if datatype is not None:
+            return datatype
+        
         return numpy.dtype(float)
-#        tie_points = self.tie_points
-#        datatype = tie_points[0].dtype
-#        
-#        for tie_point in tie_points[1:]:
-#            datatype = numpy.result_type(datatype, tie_point.dtype)
+
+    @property
+    def ndim(self):
+        '''The number of dimensions of the uncompressed data.
+
+    **Examples:**
+
+    >>> d.shape
+    (73, 96)
+    >>> d.ndim
+    2
+    >>> d.size
+    7008
+
+    >>> d.shape
+    (1, 1, 1)
+    >>> d.ndim
+    3
+    >>> d.size
+    1
+
+    >>> d.shape
+    ()
+    >>> d.ndim
+    0
+    >>> d.size
+    1
+
+        '''
+        return self._get_component('ndim')
+
+    @property
+    def shape(self):
+        '''Shape of the uncompressed data.
+
+    **Examples:**
+
+    >>> d.shape
+    (73, 96)
+    >>> d.ndim
+    2
+    >>> d.size
+    7008
+
+    >>> d.shape
+    (1, 1, 1)
+    >>> d.ndim
+    3
+    >>> d.size
+    1
+
+    >>> d.shape
+    ()
+    >>> d.ndim
+    0
+    >>> d.size
+    1
+
+        '''
+        return self._get_component('shape')
+
+    @property
+    def size(self):
+        '''Number of elements in the uncompressed data.
+
+    **Examples:**
+
+    >>> d.shape
+    (73, 96)
+    >>> d.size
+    7008
+    >>> d.ndim
+    2
+
+    >>> d.shape
+    (1, 1, 1)
+    >>> d.ndim
+    3
+    >>> d.size
+    1
+
+    >>> d.shape
+    ()
+    >>> d.ndim
+    0
+    >>> d.size
+    1
+
+        '''
+        return self._get_component('size')
+
+    @property
+    def compressed_array(self):
+        '''Return an independent numpy array containing the compressed data.
+
+    :Returns:
+
+        `numpy.ndarray`
+            The compressed array.
+
+    **Examples:**
+
+    >>> n = a.compressed_array
+    >>> import numpy
+    >>> isinstance(n, numpy.ndarray)
+    True
+
+        '''
+        ca = self._get_compressed_Array(None)
+        if ca is None:
+            raise ValueError("There is no underlying compressed array")
+
+        return ca.array
+
+    # ----------------------------------------------------------------
+    # Methods
+    # ----------------------------------------------------------------
+    def get_sampled_axes(self):
+        '''Return axes that are sampled in the underlying array.
+
+    .. versionadded:: (cfdm) TODO
+
+    :Returns:
+
+        `list`
+            The sampled axes described by their integer positions.
+
+    **Examples:**
+
+    TODO
+
+        '''
+        TODO
+        
+#        compressed_dimension = self.get_compressed_dimension()
+#        compressed_ndim = self._get_compressed_Array().ndim
 #
-#        return datatype
+#        return list(range(
+#            compressed_dimension,
+#            self.ndim - (compressed_ndim - compressed_dimension - 1)
+#        ))
+
+    def get_sampled_dimensions(self, *default):
+        '''Return the positions of the sampled dimension in the compressed
+    array.
+
+    .. versionadded:: (cfdm) 1.7.0
+
+    .. seealso:: `get_compressed_axearray`, `get_compressed_axes`,
+                 `get_compressed_type`
+
+    :Parameters:
+
+        default: optional
+            Return *default* if the underlying array is not
+            compressed.
+
+    :Returns:
+
+        `int`
+            The position of the compressed dimension in the compressed
+            array. If the underlying is not compressed then *default*
+            is returned, if provided.
+
+    **Examples:**
+
+    >>> i = d.get_compressed_dimension()
+
+        '''
+        return self._get_component('compressed_dimension', *default)
 
     # ----------------------------------------------------------------
     # Methods
@@ -122,6 +301,6 @@ class SampledArray(abstract.Array):
     >>> b = a.to_memory()
 
         '''
-        return self
+        return 'TODO'
 
 # --- End: class
