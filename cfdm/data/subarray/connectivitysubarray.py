7808cde855b2ec6447b4f3dff47cc074570a41f0
David Hassell <david.hassell@ncas.ac.uk>
2023-05-25 17:26:26 +0100

    dev

--- cfdm/data/subarray/connectivitysubarray.py
+++ cfdm/data/subarray/connectivitysubarray.py
@@ -86,69 +86,85 @@ class ConnectivitySubarray(Subarray):
         from scipy.sparse import csr_array
 
         connectivity = self._select_data(check_mask=False)
-        shape = connectivity.shape
-        shape0 = shape[0]
+        
+        n_cells = connectivity.shape[0]
+        n_nodes = np.ma.count(connectivity)
 
-        topology_dimension = self.get_topology_dimension()
         row_ind = []
         col_ind = []
 
+        topology_dimension = self.get_topology_dimension()
         if topology_dimension == 2:
+            # Number of nodes per face, minus 1
+            n_face_nodes_minus_1 = np.ma.count(connectivity, axis=1) - 1
+
             compressed = np.ma.compressed
-            for i, nodes0 in enumerate(connectivity):
-                nodes0 = compressed(nodes0).tolist()
-                n_nodes0_minus_1 = len(nodes0) - 1
-                set_nodes0 = set(nodes0)
-                for j, nodes1 in enumerate(connectivity[i + 1 :]):
-                    nodes1 = compressed(nodes1).tolist()
-                    common = set_nodes0.intersection(nodes1)
-                    n_common = len(common)
-                    if n_common < 2:
-                        # face0 and face1 are not connected
+            for i, nodes in enumerate(connectivity):
+                nodes = compressed(nodes).tolist()
+                set_nodes = set(nodes)                
+                
+                # Find all of the edges j that possibly share an edge
+                # with face i
+                node_test = connectivity == nodes[0]
+                for node in nodes[1:]:
+                    node_test |=  connectivity == node            
+                    
+                possibly_connected_faces = set(np.where(node_test)[0].tolist())
+                possibly_connected_faces.remove(i)
+
+                # For each possibly connected face j, check to see if
+                # it really connected to face i. Faces i and j are
+                # connected if they share two or more nodes which are
+                # adjacent in both faces.
+                for j in possibly_connected_faces:
+                    nodes1 = compressed(connectivity[j]).tolist()
+                    common_nodes = set_nodes.intersection(nodes1)
+                    n_common_nodes = len(common_nodes)
+                    if n_common_nodes < 2:
+                        # Faces i and j share fewer than two nodes =>
+                        # they are not connected
                         continue
 
-                    if n_common == 2:
-                        # Check that the common nodes comprise
-                        # adjacent vertices in both faces
-                        x = common.pop()
-                        y = common.pop()
-                        diff = abs(nodes0.index(x) - nodes0.index(y))
-                        if diff == 1 or diff == n_nodes0_minus_1:
-                            # The common nodes are adjacent vertices
-                            # in face0
+                    if n_common_nodes == 2:
+                        # Faces i and j share two nodes
+                        x = common_nodes.pop()
+                        y = common_nodes.pop()
+                        diff = abs(nodes.index(x) - nodes.index(y))
+                        if diff == 1 or diff == n_face_nodes_minus_1[i]:
+                            # The two common nodes are adjacent in
+                            # face i
                             diff = abs(nodes1.index(x) - nodes1.index(y))
-                            if diff == 1 or diff == len(nodes1) - 1:
-                                # The common nodes are also adjacent
-                                # vertices in face1 => face0 and face1
-                                # are connected.
+                            if diff == 1 or diff == n_face_nodes_minus_1[j]:
+                                # The two common nodes are also
+                                # adjacent in face j => faces i and j
+                                # are connected
                                 row_ind.append(i)
-                                col_ind.append(i + 1 + j)
+                                col_ind.append(j)
                     else:
-                        # face0 and face1 are connected
-                        row_ind.append(i)
-                        col_ind.append(i + 1 + j)
-
-        elif topology_dimension == 1:
-            for i, nodes0 in enumerate(connectivity):
-                set_nodes0 = set(nodes0.tolist())
-                for j, nodes1 in enumerate(connectivity[i + 1 :]):
-                    common = set_nodes0.intersection(nodes1.tolist())
-                    if not common:
-                        # edge0 and edge1 are not connected
-                        continue
-
-                    # edge0 and edge1 are connected
+                        # TODOUGRID: 3 or more shared nodes
+                        pass
+                    
+        elif topology_dimension == 1:            
+            for i, nodes in enumerate(connectivity):                
+                # Find all of the edges j that share one or two nodes
+                # with face i
+                node_test = connectivity == nodes[0]
+                node_test |= connectivity == nodes[1]
+                connected_edges = set(np.where(node_test)[0].tolist())
+                connected_edges.remove(i)        
+                for j in connected_edges:
                     row_ind.append(i)
-                    col_ind.append(i + 1 + j)
-
+                    col_ind.append(j)
+                
         elif topology_dimension > 2:
             raise ValueError("Can't do volumes!!!!")
 
         # Create a upper diagonal sparse array, and then symmetrically
         # copy its values to the lower diagonal.
         data = np.ones((len(row_ind),), dtype=bool)
-        c = csr_array((data, (row_ind, col_ind)), shape=(shape0, shape0))
+        c = csr_array((data, (row_ind, col_ind)), shape=(n_cells, n_cells))
         c = c[indices]
+        # TODOUGRID: what if c[indices] does not create suitable square?
 
         return c + c.T
 
