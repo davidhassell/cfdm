5696dead8cf230de6cfd9e52042108705eda91d3
David Hassell <davidhassell@users.noreply.github.com>
2026-08-21 16:22:46 +0100

    Typos
    Co-authored-by: Sadie L. Bartholomew <sadie.bartholomew@ncas.ac.uk>

--- cfdm/read_write/netcdf/netcdfread.py
+++ cfdm/read_write/netcdf/netcdfread.py
@@ -1023,7 +1023,7 @@ class NetCDFRead(IORead, FieldChecker, NetCDFCheckerMixin):
 
             cfa_backend_options: `None` or `dict`, optional
                 The options to use with each backend when opening an
-                aggregated dataset.See `cfdm.read` for details.
+                aggregated dataset. See `cfdm.read` for details.
 
                 .. versionadded:: (cfdm) NEXTVERSION
 
@@ -1255,7 +1255,7 @@ class NetCDFRead(IORead, FieldChecker, NetCDFCheckerMixin):
             backend_options = {}
         elif not isinstance(backend_options, dict):
             raise ValueError(
-                "'backend_options' parameter must be a dictionary or None"
+                "'backend_options' parameter must be a dictionary or None. "
                 f"Got: {backend_options!r}"
             )
         else:
@@ -1264,7 +1264,7 @@ class NetCDFRead(IORead, FieldChecker, NetCDFCheckerMixin):
             valid_options = [f"{b}_options" for b in xnetcdf.backends]
             if not set(backend_options).issubset(valid_options):
                 raise ValueError(
-                    "Invalid key in 'backend_options' dictionary"
+                    "Invalid key in 'backend_options' dictionary. "
                     f"Got {backend_options!r}, expected a subset "
                     f"of {valid_options}"
                 )
@@ -1629,7 +1629,7 @@ class NetCDFRead(IORead, FieldChecker, NetCDFCheckerMixin):
 
         # ------------------------------------------------------------
         # Find the CF version for the file
-        # -------------------------------ding -----------------------------
+        # ------------------------------------------------------------
         Conventions = g["global_attributes"].get("Conventions", "")
 
         # If the string contains any commas, it is assumed to be a
@@ -1666,7 +1666,7 @@ class NetCDFRead(IORead, FieldChecker, NetCDFCheckerMixin):
         if g["CF>=1.11"] and g["UGRID_version"] is None:
             g["UGRID_version"] = "1.0"
 
-        # The netCDF4 dataset objects that have been opened (i.e. the
+        # The netCDF4 dataset objects that have been opened (i.e.
         # for parent file and any external datasets)
         g["datasets"] = [nc]
 
@@ -5905,8 +5905,8 @@ class NetCDFRead(IORead, FieldChecker, NetCDFCheckerMixin):
                 return kwargs
 
             if backend == "xarray":
-                # For xarray, we don't wnt to create an `XnetcdfArray`
-                # instance, because 'variable' will wither contain ist
+                # For xarray, we don't want to create an `XnetcdfArray`
+                # instance, because 'variable' will wither contain its
                 # own Dask array (if the xarray 'chunks' argument was
                 # not `None`), or a `numpy` array (or similar).
                 array = variable.backend_accessor.data
