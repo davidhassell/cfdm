2e604e05d3044cf583930526f00a183763fd74d0
David Hassell <david.hassell@ncas.ac.uk>
2026-08-21 16:44:26 +0100

    Check 'closest_ancestor'

--- cfdm/test/test_zarr.py
+++ cfdm/test/test_zarr.py
@@ -291,6 +291,9 @@ class read_writeTest(unittest.TestCase):
         self.assertTrue(z.equals(n))
         self.assertTrue(z.equals(f))
 
+        # Check 'closest_ancestor'
+        cfdm.read(grouped_file, group_dimension_search="closest_ancestor")
+
     def test_zarr_groups_geometry(self):
         """Test Zarr groups containing cell geometries."""
         f = cfdm.example_field(6)
